79e48ae7db990cace183f87324189ab55f5c5387
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-02-19 01:52:58 +0000

    fixed things

--- src/js/showCart.js
+++ src/js/showCart.js
@@ -1,13 +1,13 @@
-import { getlocalStorage } from "./services";
+import { getlocalStorage, setlocalStorage } from "./services";
 import { createFruitCart } from "./fruitBanner"
 
 export async function showCart() {
     const cartInLocal = getlocalStorage("cart");
     let total = 0;
+    const totalPrice = document.getElementById("total-price");
 
     for (let i = 0; i < cartInLocal.length; i++) {
         createFruitCart(cartInLocal[i]);
-        const totalPrice = document.getElementById("total-price");
 
         if (totalPrice.innerText === "XXXX.XXXX") {
             total = cartInLocal[i].totalPrice;
@@ -20,3 +20,8 @@ export async function showCart() {
 }
 
 document.addEventListener("DOMContentLoaded", showCart);
+const buyBtn = document.getElementById("buy-button");
+    buyBtn.addEventListener("click", 
+        setlocalStorage("total-price", totalPrice.innerText),
+        window.location.search = "order-cart/buy-form.html"
+    )
