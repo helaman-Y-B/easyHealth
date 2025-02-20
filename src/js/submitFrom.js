22f6467174e65ba2a83d350fb20124cc0ee45f23
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-02-20 01:24:57 +0000

    Added the removeLocalStorage function in the submitform.

--- src/js/submitFrom.js
+++ src/js/submitFrom.js
@@ -1,10 +1,12 @@
 import { formValidation } from "./eventListeners";
+import { removeLocalStorage } from "./services"
 
 function submitAction() {
     const btn = document.getElementById("buy-btn");
     btn.addEventListener("click", () => {
         if (formValidation()) {
             alert("Order Submitted");
+            removeLocalStorage("cart");
             document.getElementById("order-form").submit();
         }
     });
